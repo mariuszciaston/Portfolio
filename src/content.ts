b6f5043f984a12b43d370b0029626a7c8471a746
Mariusz <casmar90@gmail.com>
2024-06-19 15:30:42 +0200

    Extract content to separate file

---
+++ src/content.ts
@@ -0,0 +1,208 @@
+const webDevProjects = [
+	{
+		href: 'https://mariuszciaston.github.io/Battleship/',
+		imgSrc: 'webdev/Battleship.png',
+		title: 'Battleship',
+		year: '2024',
+		description: 'gra w statki',
+	},
+	{
+		href: 'https://mariuszciaston.github.io/minimaLIST/',
+		imgSrc: 'webdev/minimaList.png',
+		title: 'minimaLIST',
+		year: '2023',
+		description: 'aplikacja typu lista zadań',
+	},
+
+	{
+		href: 'https://mariuszciaston.github.io/Weather-App/',
+		imgSrc: 'webdev/Weather-App.png',
+		title: 'Weather App',
+		year: '2023',
+		description: 'aplikacja pogodowa',
+	},
+	{
+		href: 'https://mariuszciaston.github.io/Tic-Tac-Toe/',
+		imgSrc: 'webdev/Tic-Tac-Toe.png',
+		title: 'Tic Tac Toe',
+		year: '2023',
+		description: 'gra kółko i krzyżyk',
+	},
+	{
+		href: 'https://mariuszciaston.github.io/Sign-Up_Form/',
+		imgSrc: 'webdev/Sign-Up_Form.png',
+		title: 'Sign-Up Form',
+		year: '2023',
+		description: 'prototyp formularza rejestracyjnego',
+	},
+	{
+		href: 'https://mariuszciaston.github.io/Admin-Dashboard/',
+		imgSrc: 'webdev/Admin-Dashboard.png',
+		title: 'Admin Dashboard',
+		year: '2023',
+		description: 'prototyp panelu administracyjnego',
+	},
+
+	{
+		href: 'https://mariuszciaston.github.io/Library/',
+		imgSrc: 'webdev/Library.png',
+		title: 'Library',
+		year: '2022',
+		description: 'aplikacja do zapisywania książek',
+	},
+
+	{
+		href: 'https://mariuszciaston.github.io/Etch-A-Sketch/',
+		imgSrc: 'webdev/Etch-A-Sketch.png',
+		title: 'Etch-A-Sketch',
+		year: '2022',
+		description: 'aplikacja do tworzenia pixel artu',
+	},
+
+	{
+		href: 'https://mariuszciaston.github.io/Calculator/',
+		imgSrc: 'webdev/Calculator.png',
+		title: 'Calculator',
+		year: '2022',
+		description: 'aplikacja kalkulator',
+	},
+
+	{
+		href: 'https://mariuszciaston.github.io/Rock-Paper-Scissors/',
+		imgSrc: 'webdev/Rock-Paper-Scissors.png',
+		title: 'Rock Paper Scissors',
+		year: '2022',
+		description: 'gra papier, kamień, nożyce',
+	},
+	{
+		href: 'https://sdn.org.pl/',
+		imgSrc: 'webdev/Sdn.org.pl.png',
+		title: 'Sdn.org.pl',
+		year: '2022',
+		description: 'strona internetowa Stowarzyszenia Dobrej Nadziei',
+	},
+	{
+		href: 'https://film.asp.krakow.pl/',
+		imgSrc: 'webdev/Film.asp.krakow.pl.png',
+		title: 'Film.asp.krakow.pl',
+		year: '2020',
+		description: 'strona internetowa Pracowni Filmu Animowanego ASP Kraków',
+	},
+];
+
+const mocapProjects = [
+	{
+		href: 'mocap/mocap-1-large.jpg',
+		imgSrc: 'mocap/mocap-1.jpg',
+		description: 'mocap-1.jpg',
+	},
+	{
+		href: 'mocap/mocap-2-large.jpg',
+		imgSrc: 'mocap/mocap-2.jpg',
+		description: 'mocap-2.jpg',
+	},
+	{
+		href: 'mocap/mocap-3-large.jpg',
+		imgSrc: 'mocap/mocap-3.jpg',
+		description: 'mocap-3.jpg',
+	},
+	{
+		href: 'mocap/mocap-4-large.jpg',
+		imgSrc: 'mocap/mocap-4.jpg',
+		description: 'mocap-4.jpg',
+	},
+	{
+		href: 'mocap/mocap-5-large.jpg',
+		imgSrc: 'mocap/mocap-5.jpg',
+		description: 'mocap-5.jpg',
+	},
+
+	{
+		iframeSrc: 'https://www.youtube.com/embed/C-MedZPyLog?si=bJ5w_yK_ckT8XVFv',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/LtybBucgNmI?si=0WuRXA3Aj1RNBdCM',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/btZNWk0gGd4?si=LPYkdopqCibfIml5',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/ozqPbTbIxMs?si=tglL0KkauTrHXJKO',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/7TioWVaoksQ?si=_ZAvdA3bT73ygp7b',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/ev983G6xkdE?si=T_TCLi2h3_s3Rbbv',
+	},
+	{
+		iframeSrc: 'https://www.youtube.com/embed/ZAZwuitd4_8?si=IcKpuSrWjHsyfBX3',
+	},
+];
+
+const musicProjects = [
+	{
+		iframeSrc:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1822538817&color=%23000000&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+		title: 'Strasznie',
+		year: '2016',
+		description: 'eksperymentalny utwór elektroniczny',
+	},
+	{
+		iframeSrc:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1822538397&color=%23000000&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+		title: 'Nightmare',
+		year: '2012',
+		description: 'Dark dubstep',
+	},
+	{
+		iframeSrc:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1822538145&color=%23000000&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+		title: 'You Like It Remix',
+		year: '2008',
+		description: 'Electro house',
+	},
+	{
+		iframeSrc:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1822537608&color=%23000000&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+		title: 'Friday Night',
+		year: '2006',
+		description: 'Tribal techno',
+	},
+];
+
+const graphicProjects = [
+	{
+		href: 'graphics/Candis-Konopie-2022-Zielona-Ulotka-DL-large.png',
+		imgSrc: 'graphics/Candis-Konopie-2022-Zielona-Ulotka-DL.png',
+		title: 'Candis',
+		year: '2022',
+		description: 'Ulotka DL',
+	},
+
+	{
+		href: 'graphics/Uzaleznienia-Behawioralne-2018-Batorego-Ulotka-DL-large.png',
+		imgSrc: 'graphics/Uzaleznienia-Behawioralne-2018-Batorego-Ulotka-DL.png',
+		title: 'Uzależnienia Behawioralne',
+		year: '2018',
+		description: 'Ulotka DL',
+	},
+
+	{
+		href: 'graphics/Candis-Konopie-2021-Czarna-Ulotka-DL-large.png',
+		imgSrc: 'graphics/Candis-Konopie-2021-Czarna-Ulotka-DL.png',
+		title: 'Candis',
+		year: '2021',
+		description: 'Ulotka DL',
+	},
+
+	{
+		href: 'graphics/Uzaleznienia-Behawioralne-2019-Batorego-Ulotka-DL-large.png',
+		imgSrc: 'graphics/Uzaleznienia-Behawioralne-2019-Batorego-Ulotka-DL.png',
+		title: 'Uzależnienia Behawioralne',
+		year: '2019',
+		description: 'Ulotka DL',
+	},
+];
+
+export { webDevProjects, mocapProjects, musicProjects, graphicProjects };
